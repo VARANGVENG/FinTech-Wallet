820d46c71258ed53cf82aca108ddf741be74a0a6
VARANGVENG <vengvarang8@gmail.com>
2026-06-18 22:50:51 +0700

    Initial commit

---
+++ lib/features/authentication/presentation/pages/login_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      // appBar: AppBar(
+      //   backgroundColor: const Color.fromARGB(255, 141, 82, 180),
+      //   leading: Icon(Icons.back_hand_sharp),
+      //   title: Center(
+      //     child: Text(
+      //       'Fintech Wallet',
+      //       style: TextStyle(color: Colors.black, fontSize: 24),
+      //     ),
+      //   ),
+      //   actions: [Icon(Icons.back_hand_sharp)],
+      // ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(color: Colors.blue, height: 200, width: 500),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 40, left: 40),
+                child: Positioned(
+                  child: Icon(Icons.access_alarm),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
